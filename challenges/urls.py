a204e83a90f0f42cb98477de55f1f58bc183c14b
C K Ashby <clay.ashby@gmail.com>
2024-12-26 14:06:52 -0800

    add index view to list monthly challenges with links

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     # path('jan', views.jan, name='jan'),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"), 
