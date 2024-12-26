ddb9fe1a0e2cb70a4789d7f4027f7f0b5a20569e
C K Ashby <clay.ashby@gmail.com>
2024-12-26 11:02:12 -0800

    remove unused views from students app

--- students/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse('Hola estudiantes!')
-
-def jan(request):
-    return HttpResponse('Este es el reto de enero')
