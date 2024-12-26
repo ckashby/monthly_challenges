27eddb59a0602908b46dbbde6fbc6a48a9745ae7
C K Ashby <clay.ashby@gmail.com>
2024-12-26 11:00:47 -0800

    refactor challenges app to use dynamic monthly challenges

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('jan', views.jan, name='jan'),
+    # path('', views.index, name='index'),
+    # path('jan', views.jan, name='jan'),
+    # path('feb', views.feb, name='feb'),
+    path("<month>", views.monthly_challenge),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create a view for index page
+# def index(request):
+#     return HttpResponse("Hola amigos!")
 
+def monthly_challenge(request, month):
+    if month == 'jan':
+        challenge_text = "This is the January challenge"
+    elif month == 'feb':
+        challenge_text = "This is the February test"
+    elif month == 'mar':
+        challenge_text = "This is the March goal"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
 
-def index(request):
-    return HttpResponse("Hola amigos!")
-
-def jan(request):
-    return HttpResponse("This is the January page")
