ad94be14ccdcb7c8c874a57c3992fb5f50ed7c7b
C K Ashby <clay.ashby@gmail.com>
2024-12-27 09:55:29 -0800

    remove named routes from dogs URL patterns for simplification

--- dogs/urls.py
+++ dogs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('jan', views.jan, name='jan'),
+    path('', views.index),
+    path('jan', views.jan),
 ]
